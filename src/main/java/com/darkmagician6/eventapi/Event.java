ed129e2fd4f0adbb928b557c26471bf80ac581f4
vlouboos <vlouyearlinjinhua@outlook.com>
2022-02-26 21:05:21 +0800

    创建环境

---
+++ src/main/java/com/darkmagician6/eventapi/Event.java
@@ -0,0 +1,13 @@
+package com.darkmagician6.eventapi;
+
+public class Event {
+    public boolean cancelled;
+
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+}
